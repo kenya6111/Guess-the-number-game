278909d1259cc5c3a5125efd4037f3e70ed08b30
kenya6111 <98050153+kenya6111@users.noreply.github.com>
2023-02-28 15:43:46 +0900

    initial commit
    数あてのゲームです。練習です。

---
+++ hello_jupiter.py
@@ -0,0 +1,14 @@
+import random;
+
+
+num=random.randint(0,10);
+
+for i in range(1,5):
+    print(i,'kaime')
+    aaa=int(input())
+    
+    if num==aaa:
+        print('atari!!')
+    else:
+        print('hazure!!')
+    
