d75f59246cded0d08bcc87e8d4e6db2939989ac8
kenya6111 <98050153+kenya6111@users.noreply.github.com>
2024-06-30 23:05:50 +0900

    Update hello_jupiter.py

--- hello_jupiter.py
+++ hello_jupiter.py
@@ -8,43 +8,45 @@ def get_random_number(min_value, max_value):
     # min_valueとmax_valueの間のランダムな整数を生成する
     return random.randint(min_value, max_value)
 
-min_value = get_input("最小値を入力してください")
-max_value = get_input("最大値を入力してください")
+def guess_number_game():
+    min_value = get_input("最小値を入力してください")
+    max_value = get_input("最大値を入力してください")
 
-# 最小値が最大値より大きくないか確認
-while min_value > max_value:
-    if min_value > max_value:
-        print("入力値が不正です。最小値が最大値より大きいです")
-        min_value=get_input("最小値を入力してください")
-        max_value = get_input("最大値を入力してください")
+    # 最小値が最大値より大きくないか確認
+    while min_value > max_value:
+        if min_value > max_value:
+            print("入力値が不正です。最小値が最大値より大きいです")
+            min_value = get_input("最小値を入力してください")
+            max_value = get_input("最大値を入力してください")
 
-# 回答制限回数
-inputLimitTimes=5
-# 現在の回答回数カウント
-currInputTime=1
+    # 答えを取得
+    target = get_random_number(min_value, max_value)
 
-target = get_random_number(min_value, max_value)
+    print(f"{min_value}~{max_value}の中から正解の数字を当ててください")
 
-print(str(min_value)+"~"+str(max_value)+"の中から正解の数字を当ててください")
+    # 回答制限回数
+    input_limit_times=5
+    # 現在の回答回数カウント
+    curr_input_time=1
 
-answer = int(input("1回目!"))
+    while curr_input_time <= input_limit_times:
 
-while(answer != target):
-    if(currInputTime>inputLimitTimes):
-        print("回答制限回数を超えました。残念でした！")
-        break
+        answer = get_input(f"{curr_input_time}回目！数字を当ててください: ")
 
-    if(answer == target):
-        print("あたりです！")
-        break
-    elif(answer > target):
-        print("もっと小さいです！")
-    else: 
-        print("もっと大きいです")
+        if answer == target :
+            print("あたりです！")
+            break
+        elif answer > target :
+            print("もっと小さいです!")
+        else: 
+            print("もっと大きいです!")
 
-    answer = int(input(str(currInputTime)+"回目！..."))
-    currInputTime+=1
+        curr_input_time += 1
 
-print("答えは..." + str(target))
+        if(curr_input_time > input_limit_times):
+            print("回答制限回数を超えました。残念でした！")
 
+    print("答えは..." + str(target))
 
+if __name__ == "__main__":
+    guess_number_game()
