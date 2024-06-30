17134ed889ae51b6ad0543973a23f648cd81a469
kenya6111 <98050153+kenya6111@users.noreply.github.com>
2024-06-30 21:03:26 +0900

    Update hello_jupiter.py

--- hello_jupiter.py
+++ hello_jupiter.py
@@ -1,14 +1,26 @@
+import sys;
 import random;
 
+min = int(input("最小値を入力してください"))
+max = int(input("最大値を入力してください"))
 
-num=random.randint(0,10);
+while min > max:
+    if min > max:
+        max=int(input("最小値より大きい最大値を設定してください"))
 
-for i in range(1,5):
-    print(i,'kaime')
-    aaa=int(input())
+target = int(random.uniform(min, max))
+
+answer = int(input("数字を当ててください"))
+inputLimitTimes=5
+currInputTime=0
+while(answer != target):
+    if(currInputTime>inputLimitTimes):
+        print("制限回数を超えました。残念でした！")
+        break
+
+    answer = int(input("残念！数字を当ててください"))
+    currInputTime+=1
     
-    if num==aaa:
-        print('atari!!')
-    else:
-        print('hazure!!')
     
+
+print("答えは..." + str(target))
